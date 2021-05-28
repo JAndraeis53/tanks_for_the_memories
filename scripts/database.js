34e35c74dab53f05aa796010d02a0f9e05743eba
Jalon Duncan <duncanjalon@gmail.com>
2021-05-28 13:29:19 -0500

    first attempt at javascript

---
+++ scripts/database.js
@@ -0,0 +1,24 @@
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            food: "crustaceans",
+            size: 3
+        },
+        {
+            name: "Angel",
+            food: "limes",
+            size: 2
+        },
+        {
+            name: "Tony",
+            food: "anything",
+            size: 9
+        },
+        {
+            name: "Roberto",
+            food: "pelletes",
+            size: 1
+        }
+    ]
+}
---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+import {} from ''
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+const parentHTMLElement = document.querySelector("insert selector here")
+
+
+parentHTMLElement.innerHTML = FishList()
